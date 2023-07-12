25ed4f40d4cb9682cba1caaa94802a5f62159a8e
CRIMX <straybugs@gmail.com>
2023-07-13 01:48:04 +0800

    refactor: update value-enhancer

--- src/lib/context.ts
+++ src/lib/context.ts
@@ -1,6 +1,6 @@
 import { setContext, getContext } from "svelte";
 import type { I18n, LocaleLangObservable, TFunctionObservable } from "val-i18n";
-import { unwrap, derive, val, type Val } from "value-enhancer";
+import { unwrap, val, type Val } from "value-enhancer";
 
 const I18nContextKey = "_ValI18n";
 const I18nTContextKey = "_ValI18nT";
@@ -15,8 +15,14 @@ export type I18nObservable = Val<I18n>;
  */
 export const setI18n = (i18n: I18n): I18nObservable => {
   const i18n$ = setContext(I18nContextKey, val(i18n));
-  setContext(I18nTContextKey, unwrap(derive(i18n$, (i18n) => i18n.t$)));
-  setContext(I18nLangContextKey, unwrap(derive(i18n$, (i18n) => i18n.lang$)));
+  setContext(
+    I18nTContextKey,
+    unwrap(i18n$, (i18n) => i18n.t$)
+  );
+  setContext(
+    I18nLangContextKey,
+    unwrap(i18n$, (i18n) => i18n.lang$)
+  );
   return i18n$;
 };
 
